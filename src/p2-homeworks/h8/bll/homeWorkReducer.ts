c3d648552956c8486402b0938154831395013e67
kitty-sam <www.moonlight@mail.ru>
2021-11-29 12:02:27 +0300

    add hometask#8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,66 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+
+type ActionsType = sortStateACType | checkStateACType
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionsType): UserType[] => {
+
+    const  CompareFnUp = (a: UserType, b: UserType) => {
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    }
+    const  CompareFnDown = (a: UserType, b: UserType) => {
+        if (a.name > b.name) {
+            return -1;
+        }
+        if (a.name < b.name) {
+            return 1;
+        }
+        return 0;
+    }
+
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            let stateCopy = [...state]
+
+       return     action.payload === "up" ? stateCopy.sort(CompareFnUp) : stateCopy.sort(CompareFnDown)
+
         }
-        case 'check': {
-            // need to fix
+        case 'check':
+
+          return  state.filter((person : UserType) => {
+                return person.age >= action.payload
+            })
+
+
+
+
+        default:
             return state
-        }
-        default: return state
     }
 }
+
+const sortStateAC = (payload: string) => {
+    return {
+        type: "sort",
+        payload: payload
+    } as const
+
+}
+
+const checkStateAC = (payload: number) => {
+    return {
+        type: "check",
+        payload: payload
+    } as const
+
+}
+
+type sortStateACType = ReturnType<typeof sortStateAC>
+type checkStateACType = ReturnType<typeof checkStateAC>
